5e96acd7d343b38ae3f83b351123a9b09dda7ac1
unknown <nduross1@gmail.com>
2019-12-15 18:31:48 -0800

    added packages

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import TabScreen from './src/screens/TabScreen';
+
+export default class App extends Component {
+  render() {
+    return <TabScreen />;
+  }
+}
---
+++ src/config/rest_consfig.js
@@ -0,0 +1,4 @@
+export const articles_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'ng';
+export const category = 'general';
+export const _api_key = 'ee1cbf26036e43a9a26051a2fccc4ce3';
